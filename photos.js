19c03b9dd8f959a41e1b031d0722a365ee10fad5
Ivanna Zonbiv <ivannazobniv333@gmail.com>
2023-12-07 02:13:41 +0200

    homework_4

---
+++ photos.js
@@ -0,0 +1,15 @@
+/*
+Список фотографій
+*/
+var photos = new Array
+(
+	//Ettlingen,Germany
+	"ettlingen01.jpg",
+	"ettlingen02.jpg",
+	"ettlingen03.jpg",
+	
+	//Helsinki,Finland
+	"helsinki01.jpg",
+	"helsinki02.jpg",
+	"helsinki03.jpg"
+);
